947ae2af9721168b8c687aa6bb1c11a3c78a0c86
Dan <daniel_coburn@my.uri.edu>
2017-10-23 12:06:57 -0400

    skeleton code for ghosts & pacman class

--- src/Pacman.java
+++ src/Pacman.java
@@ -8,6 +8,41 @@
  *
  * @author Ryan Kelley, Dan Coburn, Jake Ward, Marcos Martinez, Nelson Herrera, Michelle Monet
  */
-public class Pacman {
+public class Pacman 
+        implements DrawableInterface
+        
+        
+    
+    
+    public void paintPacman(pacmanGraphic)
+    {
+        
+        
+        if (keyDownPushed) //calls paint then move method, moves it down the screen (positive integer),
+        {
+            //need to call method that paints pacman facing down
+            void moveBy(0, int dY)
+        }
+
+        if (keyUpPushed) //calls paint then move method, moves it up the screen (negative integer)
+        {
+            //need to call method that paints pacman facing up
+            void moveBy(0, int -dY)
+        }
+        
+        if (keyLeftPushed) //calls paint then move method, moves it left (negative integer)
+        {
+            //need to call method that paints pacman facing up
+            void moveBy(int -dX, 0)
+        }
+
+        if (keyRightPushed) //calls paint then move method, moves it right (positive integer)
+        {
+            //need to call method that paints pacman facing up
+            void moveBy(int dX, int 0)
+        }
+        
+    }
+    
     
 }
